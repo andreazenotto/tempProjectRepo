02f5e35b05c14af84b4b5b82a69fc9a7d5958fd2
Andrea Zenotto s327473 <s327473@studenti.polito.it>
2025-05-15 15:58:16 +0200

    10

--- mesothelioma_project/src/wsi_utils.py
+++ mesothelioma_project/src/wsi_utils.py
@@ -5,6 +5,7 @@ from skimage.color import rgb2hsv
 from PIL import Image
 from itertools import product
 from tqdm import tqdm
+import cv2
 
 
 def load_wsi(file_path):
@@ -12,12 +13,13 @@ def load_wsi(file_path):
     return slide
 
 
-def segment_tissue(hsv_img, saturation_threshold=0.2):
-    # Saturation is located in the second channel of the HSV image
-    saturation = hsv_img[:, :, 1]
-    # Create a binary mask based on the saturation threshold
-    tissue_mask = saturation > saturation_threshold
-    return tissue_mask
+def isWhitePatch(patch, satThresh=5):
+    patch_hsv = rgb2hsv(patch)
+    return True if np.mean(patch_hsv[:,:,1]) < satThresh else False
+
+
+def isBlackPatch(patch, rgbThresh=40):
+    return True if np.all(np.mean(patch, axis = (0,1)) < rgbThresh) else False
 
 
 def extract_patches(slide, save_dir, level, patch_size=(224, 224), tissue_threshold=0.6):
@@ -35,20 +37,14 @@ def extract_patches(slide, save_dir, level, patch_size=(224, 224), tissue_thresh
         # Extract the image region
         region = slide.read_region((x, y), level, patch_size).convert("RGB")
         region = np.array(region)
+
+        if isWhitePatch(region) or isBlackPatch(region):
+            continue
         
-        # Segment and create the patch mask
-        patch_hsv = rgb2hsv(region)
-        patch_mask = segment_tissue(patch_hsv)
-        
-        # Calculate the tissue coverage percentage in the patch
-        tissue_coverage = np.sum(patch_mask) / (patch_height * patch_width)
-        
-        # If the tissue coverage is sufficient, save the patch
-        if tissue_coverage >= tissue_threshold:
-            patch = Image.fromarray(region)
-            # Save the patch as a PNG image
-            patch_filename = f"patch_{x}_{y}.png"
-            patch.save(os.path.join(save_dir, patch_filename))
+        patch = Image.fromarray(region)
+        # Save the patch as a PNG image
+        patch_filename = f"patch_{x}_{y}.png"
+        patch.save(os.path.join(save_dir, patch_filename))
 
 
 def load_patches(directory):
